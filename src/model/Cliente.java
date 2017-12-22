9f9dd09285e5cb7e88fc8466969952ca3dc81707
jordy <jordyfaria0@gmail.com>
2017-12-22 18:00:42 -0200

    first

---
+++ src/model/Cliente.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Cliente {
+
+	private String nome;
+	private String telefone;
+	private String celular;
+	private String email;
+	private Pet pet;
+	private boolean whatsapp;
+	
+
+	
+}
---
+++ src/model/Pet.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Pet {
+
+	private String nome;
+	private char especie;
+	private String porte;
+	private String raca;
+	private String alergia;
+	private String problemaSaude;
+	private String observacao;
+	private char sexo;
+	private String medicamento;
+	private boolean perfume;	
+	
+}
